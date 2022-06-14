b4ac5bebc233591fb39da23b370de0d8582d49ef
Edison Chimezie <edison.chimezie01@gmail.com>
2022-06-13 19:15:41 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,52 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: destination of the string
+ * @src: source of the string
+ * Return: string
  */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- * _strcpy - copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest
- * @dest: copy source to this buffer
- * @src: this is the source to copy
- * Return: copy of original source
- */
-
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+char *guard = dest;
 
-	for (i = 0; i <= _strlen(src); i++)
-		dest[i] = src[i];
-
-	return (dest);
-}
-
-/* another method using pointer notation
-char *_strcpy(char *dest, char *src)
+while (*src)
 {
-        while (*src)
-	{
- 	   *dest = *src;
-	   src++;
-	   dest++;
-	}
-	return (dest);
+*dest++ = *src++;
+*dest = 0;
 }
-*/
+return (guard);
+}
